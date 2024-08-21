bd13bcdc76a4c93137514da7d3bfc6ec240311a3
harshit_garg <114936100+Harshit-880@users.noreply.github.com>
2024-08-21 18:35:13 +0530

    Update settings.py

--- greylabs/settings.py
+++ greylabs/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$a3(39diwf)kdnd_$h$lgzr1$jx5c10joo%&l50tss#2_-b581'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://grey-lab.onrender.com]
 
 
 # Application definition
